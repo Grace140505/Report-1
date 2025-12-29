9531e06242deae0cf2cbfad8f390fd0fd575ee10
Grace140505 <ecyht4@nottingham.edu.my>
2025-12-29 12:20:23 +0800

    Arduino_Week2code_3sensors.ino

--- Arduino_Week2code_3sensors_final.ino
+++ Arduino_Week2code_3sensors_final.ino
@@ -38,7 +38,7 @@ const int SLOW_L  = 80;
 const int TURN_SPEED = 200;
 
 // ================= Distance =================
-const float TICKS_PER_REV = 22.0;
+const float TICKS_PER_REV = 20.0;
 const float WHEEL_CIRC = 21.3;
 const float CM_PER_TICK = (WHEEL_CIRC / TICKS_PER_REV);
 
